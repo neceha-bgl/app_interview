db170c5bca416f7bf36bb2e9eede0876f0b88fb8
neceha bgl <neceha.bgl@gmail.com>
2013-05-27 18:05:24 +0200

    restrict access to the web interface for adding oauth authorized applications

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -4,16 +4,14 @@ Doorkeeper.configure do
   orm :active_record
 
   # This block will be called to check whether the resource owner is authenticated or not.
-  resource_owner_authenticator do
+  resource_owner_authenticator do |routes|
     User.find_by_remember_token(cookies[:remember_token]) || redirect_to(routes.signin_url(return_to: request.fullpath))
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
-  #   # Put your admin authentication logic here.
-  #   # Example implementation:
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
-  # end
+  admin_authenticator do |routes|
+    User.find_by_remember_token(cookies[:remember_token]).admin? || redirect_to(routes.signin_url(return_to: request.fullpath))
+  end
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes
